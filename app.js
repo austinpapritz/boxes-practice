77e3e3721d4368ba145425347012820e753cbddc
Austin Papritz <austin@papritz.dev>
2022-11-06 11:52:54 -0800

    add div to play-container with classList.add play-btn

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const ul = document.querySelector('#ul');
 const inputEl = document.getElementById('input');
 const btnEl = document.getElementById('btn');
 
+const playCont = document.getElementById('play-container');
+
 let arrs = ['Bees', 'Birds', 'Deer'];
 let playarrs = [];
 
@@ -14,11 +16,17 @@ let addLi = arrs.map((arr) => {
 ul.append(...addLi);
 
 btnEl.addEventListener('click', function () {
+    let playarrs = [];
     let value = inputEl.value;
+    inputEl.value = '';
     playarrs.push(value);
 
     let addDiv = playarrs.map((playarr) => {
         let div = document.createElement('div');
         div.textContent = playarr;
+        div.classList.add('play-btn');
+
+        return div;
     });
+    playCont.append(...addDiv);
 });
