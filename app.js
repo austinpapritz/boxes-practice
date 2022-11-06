f1a1d20a2932ca293271333034fbb47032833d7c
Austin Papritz <austin@papritz.dev>
2022-11-06 11:35:56 -0800

    eventlistener for btnEl, add value of input to playarrs

--- app.js
+++ app.js
@@ -3,11 +3,22 @@ const inputEl = document.getElementById('input');
 const btnEl = document.getElementById('btn');
 
 let arrs = ['Bees', 'Birds', 'Deer'];
+let playarrs = [];
 
-let nodes = arrs.map((arr) => {
+let addLi = arrs.map((arr) => {
     let li = document.createElement('li');
     li.textContent = arr;
     return li;
 });
 
-ul.append(...nodes);
+ul.append(...addLi);
+
+btnEl.addEventListener('click', function () {
+    let value = inputEl.value;
+    playarrs.push(value);
+
+    let addDiv = playarrs.map((playarr) => {
+        let div = document.createElement('div');
+        div.textContent = playarr;
+    });
+});
